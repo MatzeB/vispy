16eb3bfe0f5cac5b29abfd91fc96aa226d3df482
Matthias Braun <matze@braunis.de>
2010-03-23 13:53:18 +0100

    started working on a homepage

---
+++ homepage/make_examples.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+pushd ../examples > /dev/null
+
+# Create images
+for e in *.py; do
+	./makeexample.sh $e
+	PDFS="$PDFS `basename $e .py`.pdf"
+done
+
+popd
+
+rm -rf build
+mkdir -p build
+BUILD="build"
+EXAMPLES="$BUILD/images.part"
+for p in $PDFS; do
+	BASE="`basename $p .pdf`"
+	echo "Producing example '$BASE'"
+
+	cp ../examples/$p $BUILD
+	PDF="$BUILD/$BASE.pdf"
+	PNG="$BUILD/$BASE.png"
+	convert $PDF -resize 384x384 $PNG
+
+	SYNTAX="$BUILD/${BASE}_source.html"
+	vim -c ':let html_number_lines=0' -c ':let html_use_css=1' -c ':TOhtml' -c ":w $SYNTAX" -c ':q!' -c ':q!' ../examples/$BASE.py
+
+	cat >> $EXAMPLES << __EOF__
+	<div id="example">
+		<a href="${BASE}_source.html"><img src="images/$BASE.png"/></a>
+	</div>
+__EOF__
+done
+
